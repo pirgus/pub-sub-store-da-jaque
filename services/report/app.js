b3faf7f8a324504097d7b13184c3aa95cb83e4e6
Jaqueline Faino <pirgus@pop-os.localdomain>
2023-02-28 15:41:45 -0300

    Ainda nao consegui implementar o serviço de report

--- services/report/app.js
+++ services/report/app.js
@@ -4,6 +4,21 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 var report = {}
+
+async function processMessage(msg) {
+    console.log('processando mensagem...')
+    const data = JSON.parse(msg.content)
+    try {
+        console.log('entrou no try ?')
+        console.log(`${data}`)
+        updateReport(data)
+        printReport()
+
+    } catch (error) {
+        console.log(`X ERROR TO PROCESS: ${error.response}`)
+    }
+}
+
 async function updateReport(products) {
     for(let product of products) {
         if(!product.name) {
@@ -14,7 +29,6 @@ async function updateReport(products) {
             report[product.name]++;
         }
     }
-
 }
 
 async function printReport() {
@@ -24,7 +38,8 @@ async function printReport() {
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    console.log(`Inscrito com sucesso na fila: ${process.env.RABBITMQ_QUEUE_NAME} `) 
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)})
 } 
 
 consume()
